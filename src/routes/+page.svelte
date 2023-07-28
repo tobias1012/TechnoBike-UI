893bb81ee6b5ee525417ebb9e77bd2ae8514578b
tobias1012 <tobias1012@gmail.com>
2023-07-28 20:49:49 +0200

    added road background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import RoadBackground from "$lib/RoadBackground.svelte";
+</script>
+
+
+<div style="display: grid; grid-template-columns: repeat(2, 1fr); grid-gap: 50px;">
+    <div class="menu">yeet</div>
+    <div class="bikes">yo</div>
+</div>
+<RoadBackground />
