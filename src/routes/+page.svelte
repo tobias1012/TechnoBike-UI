bdee2bbcc02a233fc431a0e47e2f5fe6e8d75357
tobias1012 <tobias1012@gmail.com>
2023-07-28 21:06:42 +0200

    added basic structure, however more styling + bikes is needed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,31 @@
 <script lang="ts">
 	import RoadBackground from "$lib/RoadBackground.svelte";
+  let numBikes = 0;
 </script>
 
 
 <div style="display: grid; grid-template-columns: repeat(2, 1fr); grid-gap: 50px;">
-    <div class="menu">yeet</div>
-    <div class="bikes">yo</div>
+    <div class="menu">
+      <button>DataView</button>
+    </div>
+
+    <div class="bikes">
+      <h1 class="bikeTitle"> Der er {numBikes} Cykler sluttet til!</h1>
+
+    </div>
 </div>
 <RoadBackground />
+
+
+<style>
+  .menu {
+      display: grid;
+      grid-template-rows: auto;
+      grid-gap: 50px;
+    }
+
+  .bikeTitle {
+      font-size: 100%;
+      text-align: center;
+    }
+</style>
