d9f80d5353bd3be274ea947586a66f89fcbd3ddf
tobias1012 <tobias1012@gmail.com>
2023-07-28 19:07:00 +0200

    UI elements for the app

---
+++ src/lib/Biker.svelte
@@ -0,0 +1,37 @@
+<div class = "bike">            transform: translatex(5%);
+
+    <p>Name</p>
+    <div class = "bike-sprite">
+
+    </div>
+
+</div>
+
+<style lang="postcss">
+    .bike {
+        animation: shake 2s linear infinite;
+        
+    }
+    .bike-sprite {
+        background-color: blue;
+        height: 50px;
+        width: 50px;
+    }
+
+    @keyframes shake {
+		0% {
+			transform: translatex(0%);
+		}
+		33% {
+			transform: translatex(-5%);
+		}
+        66% {
+            transform: translatex(0%);
+        }
+        100% {
+            transform: translatex(5%);
+
+        }
+    }
+
+</style>
---
+++ src/lib/RoadBackground.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+
+</script>
+<div class="fixed road-container">
+    <div class="road">
+        <div class="marquee"></div>
+        <div class="marquee"></div>
+        <div class="marquee"></div>
+    </div>
+    <div class="road-delay">
+        <div class="marquee" ></div>
+        <div class="marquee" ></div>
+        <div class="marquee" ></div>
+
+    </div>
+</div>
+
+<style lang="postcss">
+    :global(body){
+        margin: 0;
+        padding: 0;
+        background: rgb(145, 145, 145);
+    }
+
+    .fixed {
+        position: fixed;
+        width: 100vw;
+        height: 100vh;
+
+    }
+
+    .road-container {
+        z-index: -2;
+    }
+
+    .road {
+        animation: marquee 10s linear infinite;
+        position: fixed;
+        top: 0;
+        left: 0;
+        height: 100%;
+        width: 100%;
+        background: rgb(145, 145, 145);
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        flex-direction: column;
+    }
+
+    .road-delay {
+        animation: marquee 10s linear 5s infinite;
+        position: fixed;
+        top: 0;
+        left: 0;
+        height: 100%;
+        width: 100%;
+        background: rgb(145, 145, 145);
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        flex-direction: column;
+    }
+    
+	.marquee {
+        background-color: white;
+        width: 50px;
+        height: 250px;
+
+	}
+
+
+	@keyframes marquee {
+		0% {
+			transform: translatey(-100%);
+		}
+		100% {
+			transform: translatey(100%);
+		}
+	}
+</style>
